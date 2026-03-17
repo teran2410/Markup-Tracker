ef6fa14bb006b5e601415304f312abdb0335f13d
Teran2410 <128594719+teran2410@users.noreply.github.com>
2026-03-17 16:38:14 -0700

    Refactor User model and update USERNAME_FIELD

--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # ROLES: Definimos quién es quién
     ROLE_CHOICES = [
         ('ING', 'Ingeniero (Admin)'),
         ('TEC', 'Técnico'),
@@ -11,16 +10,13 @@ class User(AbstractUser):
     
     role = models.CharField(max_length=3, choices=ROLE_CHOICES, default='TEC')
     
-    # NUEVOS CAMPOS: Estos no existen en Django, los creamos nosotros
     numero_empleado = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
 
-    # CONFIGURACIÓN: Cómo se comporta el modelo
-    USERNAME_FIELD = 'email'  # Se usará el correo para iniciar sesión
+    USERNAME_FIELD = 'email' 
     
     # Campos obligatorios para la creación de un superusuario (admin)
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name', 'numero_empleado']
 
     def __str__(self):
-        # Esto es lo que verás en el panel de administración
         return f"{self.first_name} {self.last_name} - {self.numero_empleado}"
