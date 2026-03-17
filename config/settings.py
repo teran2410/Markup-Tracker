9beb6eaf8ce7aa850c3a7b974377afecfb7e4fc8
Teran2410 <128594719+teran2410@users.noreply.github.com>
2026-03-17 16:39:08 -0700

    Update settings for user model and allowed origins

--- config/settings.py
+++ config/settings.py
@@ -5,25 +5,22 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SEGURIDAD: Ya no exponemos la llave en el código
 SECRET_KEY = config('SECRET_KEY')
 
-# SEGURIDAD: En producción esto será False automáticamente si no lo defines
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-# CONEXIÓN: Django ahora "habla" con Supabase vía URL
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
 
-# AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
 
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", # Puerto común de Vite/React
+    "http://localhost:5173",
     "http://localhost:3000",
 ]
 
